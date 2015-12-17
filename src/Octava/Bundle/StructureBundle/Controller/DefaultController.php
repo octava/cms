331a8c6e48eced3b04b60772ef7a669c1739bb74
Vladimir Pak <TheRatW@gmail.com>
2015-12-17 08:37:47 +0200

    seo title

--- src/Octava/Bundle/StructureBundle/Controller/DefaultController.php
+++ src/Octava/Bundle/StructureBundle/Controller/DefaultController.php
@@ -17,6 +17,16 @@ class DefaultController extends Controller
             ?: $this->get('octava_structure.config.structure_config')
                 ->getDefaultTemplate();
 
+        if ($this->container->has('sonata.seo.page')) {
+            $seoPage = $this->container->get('sonata.seo.page');
+
+            if ($structure->getTitle()) {
+                $title = $structure->getTitle().' :: '.$seoPage->getTitle();
+                $seoPage
+                    ->setTitle($title);
+            }
+        }
+
         return $this->render($template, ['structure' => $structure]);
     }
 
